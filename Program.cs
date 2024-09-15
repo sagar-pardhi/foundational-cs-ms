eff6f8e845e9cc60788f29491414758fc854dc70
sagar-pardhi <sgrpardhi007@gmail.com>
2024-09-15 12:53:43 +0530

    add: solution

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
-﻿string greeting = "Hello, World";
+﻿string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
 
-Console.WriteLine(greeting);
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
