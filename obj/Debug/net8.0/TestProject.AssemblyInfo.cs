04fbb50bc8803fa2d1ecfe31e06de52837bc2bdd
sagar-pardhi <sgrpardhi007@gmail.com>
2024-10-05 12:17:07 +0530

    completed challenge

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6294426b5a006159f05039cea3ae30057b2f9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6683603a2b5d162f48710af9ef9c404e13f074c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
